44305371eea51b178eb7d965624c91fec452919a
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-02-16 21:31:37 +0300

    actions -> delegates

--- presentation-base/src/main/java/ru/nobird/android/presentation/base/PresenterBase.kt
+++ presentation-base/src/main/java/ru/nobird/android/presentation/base/PresenterBase.kt
@@ -1,22 +1,22 @@
 package ru.nobird.android.presentation.base
 
 import androidx.annotation.CallSuper
-import ru.nobird.android.presentation.base.action.PresenterAction
+import ru.nobird.android.presentation.base.delegate.PresenterDelegate
 
 abstract class PresenterBase<V>(
     private val presenterViewContainer: PresenterViewContainer<V> = DefaultPresenterViewContainer()
 ) : DisposableViewModel(), PresenterViewContainer<V> by presenterViewContainer {
-    protected val actions: MutableList<PresenterAction<V>> = arrayListOf()
+    protected val delegates: MutableList<PresenterDelegate<V>> = arrayListOf()
 
     @CallSuper
     override fun attachView(view: V) {
         presenterViewContainer.attachView(view)
-        actions.forEach { it.attachView(view) }
+        delegates.forEach { it.attachView(view) }
     }
 
     @CallSuper
     override fun detachView(view: V) {
-        actions.forEach { it.detachView(view) }
+        delegates.forEach { it.detachView(view) }
         presenterViewContainer.detachView(view)
     }
 }
--- presentation-base/src/main/java/ru/nobird/android/presentation/base/delegate/PresenterDelegate.kt
+++ presentation-base/src/main/java/ru/nobird/android/presentation/base/delegate/PresenterDelegate.kt
@@ -1,9 +1,9 @@
-package ru.nobird.android.presentation.base.action
+package ru.nobird.android.presentation.base.delegate
 
 import ru.nobird.android.presentation.base.DisposableViewModel
 import ru.nobird.android.presentation.base.PresenterContract
 
-abstract class PresenterAction<V> : PresenterContract<V>, DisposableViewModel() {
+abstract class PresenterDelegate<V> : PresenterContract<V>, DisposableViewModel() {
     override fun attachView(view: V) {}
     override fun detachView(view: V) {}
 }
