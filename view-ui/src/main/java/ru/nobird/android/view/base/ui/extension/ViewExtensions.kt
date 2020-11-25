b97f2c1d04eb588f7cc37eafad2677312f0e8987
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-11-25 17:28:15 +0300

    add android 5 workaround

--- view-ui/src/main/java/ru/nobird/android/view/base/ui/extension/ViewExtensions.kt
+++ view-ui/src/main/java/ru/nobird/android/view/base/ui/extension/ViewExtensions.kt
@@ -32,7 +32,13 @@ inline fun View.snackbar(message: String, length: Int = Snackbar.LENGTH_SHORT, a
  */
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(windowToken, 0)
+    if (!imm.hideSoftInputFromWindow(windowToken, 0)) {
+        try {
+            imm.hideSoftInputFromWindow(null, 0)
+        } catch (_: Exception) {
+            // no op
+        }
+    }
 }
 
 /**
