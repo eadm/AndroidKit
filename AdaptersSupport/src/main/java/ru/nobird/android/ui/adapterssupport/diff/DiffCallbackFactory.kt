11a4fdb0c72a4c711f5b0a8675184cb7f6b0d19f
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-02-21 12:07:33 +0300

    remove support adapters

--- AdaptersSupport/src/main/java/ru/nobird/android/ui/adapterssupport/diff/DiffCallbackFactory.kt
+++
@@ -1,7 +0,0 @@
-package ru.nobird.android.ui.adapterssupport.diff
-
-import android.support.v7.util.DiffUtil
-
-interface DiffCallbackFactory<T> {
-    fun createDiffUtil(oldList: List<T>, newList: List<T>): DiffUtil.Callback
-}
