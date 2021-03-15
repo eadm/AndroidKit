3875f45a775c6d2939c840f592b8fb1976df4db1
RationalStargazer <rationalstargazer@gmail.com>
2021-03-15 12:34:03 +0300

    better implementation of CoroutineActionDispatcher

--- presentation-redux-coroutines/src/main/java/ru/nobird/android/presentation/redux/dispatcher/CoroutineActionDispatcher.kt
+++ presentation-redux-coroutines/src/main/java/ru/nobird/android/presentation/redux/dispatcher/CoroutineActionDispatcher.kt
@@ -1,48 +1,103 @@
 package ru.nobird.android.presentation.redux.dispatcher
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
 
 @Suppress("unused")
-abstract class CoroutineActionDispatcher<Action, Message> : ActionDispatcher<Action, Message> {
+abstract class CoroutineActionDispatcher<Action, Message>(
+    scopeConfig: ScopeConfigOptions
+) : ActionDispatcher<Action, Message> {
 
-    private val dispatcherScope = GlobalScope + Dispatchers.IO
+    private val backgroundScope: CoroutineScope = scopeConfig.createConfig().let {
+        it.backgroundScope + SupervisorJob(it.backgroundScope.coroutineContext[Job]) + it.exceptionHandler
+    }
 
     private var messageListener: ((Message) -> Unit)? = null
 
-    protected fun background(block: suspend CoroutineScope.() -> Message) {
-        dispatcherScope.launch {
-            val resultMessage = block()
-            if (isActive) {
-                MainScope().launch {
-                    // launch independent coroutine (without using of withContext())
-                    // we do not want to cancel execution at this stage if dispatcherScope will become
-                    // cancelled
-                    onNewMessage(resultMessage)
-                }
-            }
+    /**
+     * Default implementation of handleAction which uses [doActionInBackground] for all actions.
+     * Override if you want to manage background/main-thread switches manually (see [background]).
+     * Use [onNewMessage] to post result messages on main thread.
+     */
+    override fun handleAction(action: Action) {
+        backgroundScope.launch {
+            doActionInBackground(action)
         }
-    }
-
-    protected fun backgroundNoMessage(block: suspend CoroutineScope.() -> Unit) {
-        dispatcherScope.launch(block = block)
     }
 
     override fun setListener(listener: (message: Message) -> Unit) {
         messageListener = listener
     }
 
-    private fun onNewMessage(message: Message) {
-        messageListener?.invoke(message)
+    override fun cancel() {
+        backgroundScope.cancel()
+        messageListener = null
     }
 
-    override fun cancel() {
-        dispatcherScope.cancel()
+    /**
+     * Executes specified [action] on Dispatchers.IO
+     * The actions are cancellable by [cancel] call.
+     * Override doActionInBackground when you want to handle all actions in background.
+     * Use [onNewMessage] to post result messages on main thread.
+     * If you want to manage background/main-thread coroutine switches manually see [handleAction].
+     */
+    protected open suspend fun doActionInBackground(action: Action) {
     }
+
+    /**
+     * The method exists for situations when you need to start suspendable code from non-suspendable
+     * environment (mainly from custom [handleAction] implementation).
+     * Executes specified [block] in background coroutine.
+     * The coroutine is cancellable by [cancel] call.
+     */
+    protected fun background(block: suspend CoroutineScope.() -> Unit) {
+        backgroundScope.launch(block = block)
+    }
+
+    /**
+     * Posts specified [message] on main thread.
+     */
+    protected fun onNewMessage(message: Message) {
+        MainScope().launch {
+            // launch independent coroutine
+            // we do not want to cancel execution at this stage if backgroundScope will become
+            // cancelled
+            messageListener?.invoke(message)
+        }
+    }
+
+    interface ScopeConfigOptions {
+
+        val backgroundScope: CoroutineScope?
+
+        val exceptionHandler: CoroutineExceptionHandler?
+
+        fun createConfig(): ScopeConfig =
+            ScopeConfig(
+                backgroundScope ?: GlobalScope + Dispatchers.IO,
+                exceptionHandler
+                    ?: backgroundScope?.coroutineContext?.get(CoroutineExceptionHandler)
+                    ?: CoroutineExceptionHandler { _, throwable ->
+                        // log error here
+//                        Log.e(
+//                            "CoroutineActionDispatch",
+//                            "Unhandled exception in CoroutineActionDispatcher",
+//                            throwable
+//                        )
+                    }
+            )
+    }
+
+    data class ScopeConfig(
+        val backgroundScope: CoroutineScope,
+        val exceptionHandler: CoroutineExceptionHandler
+    )
 }
