17a60d72a9b1b87de2c8d88fa4715dddf7cb7f1a
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-05-07 22:45:48 +0300

    presentation-redux: migrate to kmm

---
+++ presentation-redux/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    kotlin("multiplatform")
+    id("maven-publish")
+}
+
+group = "ru.nobird.android.presentation"
+version = "1.1.1"
+
+repositories {
+    mavenCentral()
+}
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+    }
+
+    ios()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("ru.nobird.android.core:Model:1.0.7")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+        val iosMain by getting
+        val iosTest by getting
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHub"
+            url = uri("https://maven.pkg.github.com/eadm/AndroidKit")
+            credentials {
+                username = System.getenv("GITHUB_USER")
+                    ?: project.properties["GITHUB_USER"] as String?
+
+                password = System.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
+                    ?: project.properties["GITHUB_PERSONAL_ACCESS_TOKEN"] as String?
+            }
+        }
+    }
+}
